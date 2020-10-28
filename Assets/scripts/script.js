abbaa191370491c37aa6c4da11c33ae265d63143
Dazrin Tioseco <xdazee@gmail.com>
2020-10-27 22:14:56 -0700

    Changed and added  questions

--- Assets/scripts/script.js
+++ Assets/scripts/script.js
@@ -1,4 +1,4 @@
-
+//Element selectors
 var startBtn = document.getElementById('start-btn');
 var nextBtn = document.getElementById('next-btn');
 var questionContainerElement = document.getElementById('question-container');
@@ -83,36 +83,139 @@ function clearStatusClass(element) {
 
 const questions = [
     {
-        question: 'What is 2 + 2?',
+        question: "Which method would you use to find an ID element?",
         answers: [
-          { text: '4', correct: true },
-          { text: '22', correct: false }
+          { text: 'getElementById()', correct: true },
+          { text: 'getElementByID()', correct: false },
+          { text: 'getElementbyId()', correct: false },
+          { text: 'getElementsById()', correct: false },
         ]
       },
       {
-        question: 'Who is the best YouTuber?',
+        question: 'To see if two variables are equal in an if / else statement you would use ____.',
         answers: [
-          { text: 'Web Dev Simplified', correct: true },
-          { text: 'Traversy Media', correct: true },
-          { text: 'Dev Ed', correct: true },
-          { text: 'Fun Fun Function', correct: true }
+          { text: '=', correct: false },
+          { text: '==', correct: true },
+          { text: '.equals()', correct: false },
+          { text: '!=', correct: false }
         ]
       },
       {
-        question: 'Is web development fun?',
+        question: 'Math.random() returns ____.',
         answers: [
-          { text: 'Kinda', correct: false },
-          { text: 'YES!!!', correct: true },
-          { text: 'Um no', correct: false },
-          { text: 'IDK', correct: false }
+          { text: 'a number between 1 and 9', correct: false },
+          { text: 'a number between 0 and 1', correct: true },
+          { text: 'a number between 0 and 9', correct: false },
+          { text: 'a number between 0 and 99', correct: false }
         ]
       },
       {
-        question: 'What is 4 * 2?',
+        question: 'The appendChild() method places a node as the ____ child.',
         answers: [
-          { text: '6', correct: false },
-          { text: '8', correct: true }
+          { text: 'first', correct: false },
+          { text: 'last', correct: true },
+          { text: 'primary', correct: false },
+          { text: 'secondary', correct: false },
         ]
-      }
+      },
+      {
+        question: 'The first index of an array is ____.',
+        answers: [
+          { text: '1', correct: false },
+          { text: '0', correct: true },
+          { text: '-1', correct: false },
+          { text: '0 or 1', correct: false }
+        ]
+      },
+      {
+        question: 'Javascript was created by ____.',
+        answers: [
+          { text: 'Brendan Eich', correct: true },
+          { text: 'Steve Jobs', correct: false },
+          { text: 'Charles Petzold', correct: false },
+          { text: 'James Gosling', correct: false }
+        ]
+      },
+      {
+        question: 'What is not an example of an HTML event?',
+        answers: [
+          { text: 'A user pressing a button', correct: false },
+          { text: 'When the webpage loads', correct: false },
+          { text: 'A user pressing a key', correct: false },
+          { text: 'When your compiler throws an error', correct: true }
+        ]
+      },
+      {
+        question: 'Why do JavaScript and Java have similar name?',
+        answers: [
+          { text: 'JavaScript is a stripped-down version of Java', correct: false },
+          { text: 'Javascript has syntax loosely based on Java', correct: true },
+          { text: 'They both originated on the island of Java', correct: false },
+          { text: 'They were both made by the same person', correct: false }
+        ]
+      },
+      {
+        question: 'When a user views a page containing a JavaScript program, which machine actually executes the script?',
+        answers: [
+          { text: "The User's machine running a Web browser", correct: true },
+          { text: 'The Web server', correct: false },
+          { text: "A central machine deep within Netscape's corporate offices", correct: false },
+          { text: "Google's servers", correct: false }
+        ]
+      },
+      {
+        question: '______ JavaScript is also called client-side JavaScript.',
+        answers: [
+          { text: 'Microsoft', correct: false },
+          { text: 'Navigator', correct: true },
+          { text: 'LiveWire', correct: false },
+          { text: 'Native', correct: false }
+        ]
+      },
+      {
+        question: ' __________ JavaScript is also called server-side JavaScript',
+        answers: [
+          { text: 'Microsoft', correct: false },
+          { text: 'Navigator', correct: false },
+          { text: 'LiveWire', correct: true },
+          { text: 'Native', correct: false }
+        ]
+      },
+      {
+        question: 'What are variables used for in JavaScript Programs?',
+        answers: [
+          { text: 'Storing numbers, strings, or other forms of data', correct: true },
+          { text: 'A number that can mean anything', correct: false },
+          { text: 'Causing high-school algebra flashbacks', correct: false },
+          { text: 'None of the above', correct: false }
+        ]
+      },
+      {
+        question: '_____ JavaScript statements embedded in an HTML page can respond to user events such as mouse-clicks, form input, and page navigation.',
+        answers: [
+          { text: 'Server-side', correct: false },
+          { text: 'Client-side', correct: true },
+          { text: 'Local', correct: false },
+          { text: 'Native', correct: false }
+        ]
+      },
+      {
+        question: "Which of the following can't be done with client-side JavaScript?",
+        answers: [
+          { text: 'Validating a form', correct: false },
+          { text: "Sending a form's contents by email", correct: false },
+          { text: "Storing the form's contents to a database file on the server", correct: true },
+          { text: 'None of the above', correct: false }
+        ]
+      },
+      {
+        question: 'Which of the following does not follow good JavaScript variable name conventions?',
+        answers: [
+          { text: '2names', correct: false },
+          { text: '_first_and_last_names', correct: false },
+          { text: 'FirstAndLast', correct: false },
+          { text: 'All of the above', correct: true }
+        ]
+      },
         
 ]
